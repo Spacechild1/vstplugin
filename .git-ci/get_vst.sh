aaa333f67b3fbc980b71148125037f034e1ea410
IOhannes m zmölnig <zmoelnig@iem.at>
2018-10-30 15:41:22 +0100

    [ci] download VST2SDK file-by-file from JUCE repo...
    using a commit before they removed the SDK

--- .git-ci/get_vst.sh
+++ .git-ci/get_vst.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-OUTFILE=vstsdk2.zip
-SDKURL="https://download.steinberg.net/sdk_downloads/vstsdk3611_22_10_2018_build_34.zip"
 
-curl -s -o "${OUTFILE}" "${SDKURL}"
+VST2DIR=$1
+if [ "x$1" = "x" ]; then
+ VST2DIR=src/VST_SDK/VST2_SDK/pluginterfaces/vst2.x/
+fi
+VST2REPO=https://raw.githubusercontent.com/WeAreROLI/JUCE/a54535bc317b5c005a8cda5c22a9c8d4c6e0c67e/modules/juce_audio_processors/format_types/VST3_SDK/pluginterfaces/vst2.x
 
-unzip -q "${OUTFILE}" -d src
-rm -f "${OUTFILE}"
+
+mkdir -p "${VST2DIR}"
+for f in aeffect.h  aeffectx.h  vstfxstore.h; do
+  echo "fetching VST2/$f" 1>&2
+  curl -s -o "${VST2DIR}/${f}" "${VST2REPO}/${f}"
+done
