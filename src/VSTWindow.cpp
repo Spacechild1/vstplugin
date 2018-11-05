4bcad8856346e7f0c5ec0662a5618a53540c3bd5
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2018-11-05 01:00:33 +0100

    have window-factory return a w32 window on w32...

--- src/VSTWindow.cpp
+++ src/VSTWindow.cpp
@@ -1,6 +1,9 @@
 #include "VSTWindow.h"
 VSTWindow* VSTWindow::create(const std::string&name) {
   return 0
+#ifdef _WIN32
+    || (new VSTWindowWin32(name))
+#endif
 #ifdef _FOO
     || (new VSTWindowFoo(name))
 #endif
