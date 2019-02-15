580e0954fd4ea112dacfe82264d66a4de185718d
Christof Ressi <christof.ressi@student.kug.ac.at>
2019-02-15 04:06:53 +0100

    restructure project (dedicated folders for Pd and SC)

--- .git-ci/get_vst.sh
+++ .git-ci/get_vst.sh
@@ -2,7 +2,7 @@
 
 VST2DIR=$1
 if [ "x$1" = "x" ]; then
- VST2DIR=src/VST_SDK/VST2_SDK/
+ VST2DIR=vst/VST_SDK/VST2_SDK/
 fi
 VST2REPO=https://raw.githubusercontent.com/WeAreROLI/JUCE/a54535bc317b5c005a8cda5c22a9c8d4c6e0c67e/modules/juce_audio_processors/format_types/VST3_SDK/pluginterfaces/vst2.x
 
