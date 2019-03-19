d7449c0d64128cc8feae6c35587cd10f08df1def
IOhannes m zmölnig <zmoelnig@iem.at>
2019-03-19 14:35:13 +0100

    wrap the get-vst script into a function

--- .git-ci/get_vst.sh
+++ .git-ci/get_vst.sh
@@ -4,11 +4,16 @@ VST2DIR=$1
 if [ "x$1" = "x" ]; then
  VST2DIR=vst/VST_SDK/VST2_SDK/
 fi
-VST2REPO=https://raw.githubusercontent.com/WeAreROLI/JUCE/a54535bc317b5c005a8cda5c22a9c8d4c6e0c67e/modules/juce_audio_processors/format_types/VST3_SDK/pluginterfaces/vst2.x
+
+VST2ROLI=https://raw.githubusercontent.com/WeAreROLI/JUCE/a54535bc317b5c005a8cda5c22a9c8d4c6e0c67e/modules/juce_audio_processors/format_types/VST3_SDK/pluginterfaces/vst2.x
 
 
+roli2vst() {
 mkdir -p "${VST2DIR}/pluginterfaces/vst2.x/"
 for f in aeffect.h  aeffectx.h  vstfxstore.h; do
   echo "fetching $f" 1>&2
-  curl -s -o "${VST2DIR}/pluginterfaces/vst2.x/${f}" "${VST2REPO}/${f}"
+  curl -s -o "${VST2DIR}/pluginterfaces/vst2.x/${f}" "${VST2ROLI}/${f}"
 done
+}
+
+roli2vst
