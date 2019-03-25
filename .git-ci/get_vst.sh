e27205bffbfbcbe874c0766aec7d2dc8faa897bc
IOhannes m zmölnig <zmoelnig@iem.at>
2019-03-25 10:37:05 +0100

    Revert "[ci] hack around the sslcainfo problem on the w64-autobuilder"
    This reverts commit ab2b91b0e0e2a221a0dc15bc7c524513885730d9.

--- .git-ci/get_vst.sh
+++ .git-ci/get_vst.sh
@@ -20,19 +20,7 @@ done
 fst2vst() {
   mkdir -p "${VST2DIR}"
   rm -rf "${VST2DIR}"
-  # the sslcainfo thing is a hack to allow a 'git clone' with a borked git
-  # - mingw32/git: 2.19.0 (in /usr/bin/)
-  # - mingw64/git: 2.19.1.windows.1 (in /mingw64/bin/)
-  # gitlab-runner set the 'http.https://git.iem.at.sslcainfo' configuration key
-  #  to some file ("/c/Users/...")
-  # mingw64/git prefixes the filename with an additional "C:/msys64/mingw64"
-  #  but then fails to find the file and cannot clone.
-  sslcainfo=$(git config --get http.https://git.iem.at.sslcainfo)
-  git config --global --unset http.https://git.iem.at.sslcainfo
   git clone https://git.iem.at/zmoelnig/FST.git "${VST2DIR}"
-  if [ "x${sslcainfo}" != "x" ]; then
-    git config --global --add http.https://git.iem.at.sslcainfo "${sslcainfo}"
-  fi
   mkdir -p "${VST2DIR}/pluginterfaces/"
   (cd "${VST2DIR}/pluginterfaces/"; ln -s ../fst vst2.x)
 }
