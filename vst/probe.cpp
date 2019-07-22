06f9b9d91ab48722cafe59fdc5db510a1e46c53a
Christof Ressi <christof@Christofs-MacBook-Pro.local>
2019-07-22 15:12:56 +0200

    probe: supress stdout and/or stderr printout

--- vst/probe.cpp
+++ vst/probe.cpp
@@ -5,18 +5,48 @@
 
 using namespace vst;
 
+#define NO_STDOUT 1
+#define NO_STDERR 0
+
 #ifdef _WIN32
-#define MAIN wmain
-#define CHAR wchar_t
+ #define MAIN wmain
+ #define CHAR wchar_t
+ #if NO_STDOUT || NO_STDERR
+  #include <stdio.h>
+  #include <io.h>
+  #define DUP(fd) _dup(fd)
+  #define DUP2(fd, newfd) _dup2(fd, newfd)
+  const char *nullFileName = "NUL";
+ #endif
 #else
-#define MAIN main
-#define CHAR char
-#define shorten(x) x
+ #define MAIN main
+ #define CHAR char
+ #define shorten(x) x
+ #if NO_STDOUT || NO_STDERR
+  #include <stdio.h>
+  #include <unistd.h>
+  #define DUP(fd) dup(fd)
+  #define DUP2(fd, newfd) dup2(fd, newfd)
+  const char *nullFileName = "/dev/null";
+ #endif
 #endif
 
 // probe a plugin and write info to file
 // returns EXIT_SUCCESS on success, EXIT_FAILURE on fail and everything else on error/crash :-)
 int MAIN(int argc, const CHAR *argv[]) {
+    int status = EXIT_FAILURE;
+    // surpress stdout and/or stderr
+#if NO_STDOUT || NO_STDERR
+    auto nullOut = fopen(nullFileName, "w");
+#endif
+#if NO_STDOUT
+    fflush(stdout);
+    DUP2(fileno(nullOut), STDOUT_FILENO);
+#endif
+#if NO_STDERR
+    fflush(stderr);
+    DUP2(fileno(nullOut), STDERR_FILENO);
+#endif
     if (argc >= 3){
 		const CHAR *pluginPath = argv[1];
 		const CHAR *pluginName = argv[2];
@@ -32,10 +62,15 @@ int MAIN(int argc, const CHAR *argv[]) {
                     LOG_ERROR("probe: couldn't write info file");
                 }
             }
-            return EXIT_SUCCESS;
+            status = EXIT_SUCCESS;
         } catch (const Error& e){
             LOG_DEBUG("probe failed: " << e.what());
         }
 	}
-    return EXIT_FAILURE;
+#if NO_STDOUT || NO_STDERR
+    if (nullOut){
+        fclose(nullOut);
+    }
+#endif
+    return status;
 }
