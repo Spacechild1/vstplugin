08a104adc3eab72afa95ea0242573771c0ecf0e0
Christof Ressi <christof.ressi@gmx.at>
2019-08-13 05:02:47 +0200

    fix broken CI script

--- .git-ci/get_vst3.sh
+++ .git-ci/get_vst3.sh
@@ -5,7 +5,7 @@ if [ "x$1" = "x" ]; then
  VST3DIR=vst/VST_SDK/VST3_SDK/
 fi
 
-mkdir -p "${VST3DIR}/pluginterfaces/"
-rm -rf "${VST3DIR}/public.sdk/"
+rm -rf "${VST3DIR}"
+git clone https://github.com/steinbergmedia/vst3_base "${VST3DIR}/base/"
 git clone https://github.com/steinbergmedia/vst3_pluginterfaces "${VST3DIR}/pluginterfaces/"
 git clone https://github.com/steinbergmedia/vst3_public_sdk "${VST3DIR}/public.sdk/"
