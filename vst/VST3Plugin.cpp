9968b7aef4f33b597fd2e51bbde89dff4b4160f9
Christof Ressi <christof.ressi@gmx.at>
2019-09-02 18:07:46 +0200

    let's also do it for mingw64
    I've also seen some bug reports for mingw64

--- vst/VST3Plugin.cpp
+++ vst/VST3Plugin.cpp
@@ -73,7 +73,7 @@ using unichar = char16_t;
 using StringCoverter = std::wstring_convert<std::codecvt_utf8_utf16<unichar>, unichar>;
 
 static StringCoverter& stringConverter(){
-#if defined(_WIN32) && !defined(_WIN64) && defined(__MINGW32__)
+#ifdef __MINGW32__
     // because of a mingw32 bug, destructors of thread_local STL objects segfault...
     thread_local auto conv = new StringCoverter;
     return *conv;
