13447dd1243b77a0114726cf9b4af386e257fba8
Christof Ressi <info@christofressi.com>
2021-05-03 13:32:44 +0200

    bump version to 0.5-test4

--- vst/Interface.h
+++ vst/Interface.h
@@ -66,7 +66,7 @@ namespace vst {
 const int VERSION_MAJOR = 0;
 const int VERSION_MINOR = 5;
 const int VERSION_PATCH = 0;
-const int VERSION_PRERELEASE = 3;
+const int VERSION_PRERELEASE = 4;
 
 const char * getVersionString();
 
