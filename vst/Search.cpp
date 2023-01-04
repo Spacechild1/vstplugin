31f2a45d41a5a334419f80f760e3fb00f986134a
Christof Ressi <christof.ressi@gmx.at>
2023-01-04 11:41:21 +0100

    improve getBundleBinaryPath()

--- vst/Search.cpp
+++ vst/Search.cpp
@@ -75,7 +75,9 @@ const char * getBundleBinaryPath(){
     return "Contents/i386-linux";
   #elif defined(__x86_64__)
     return "Contents/x86_64-linux";
-  #else
+  #elif defined(__aarch64__)
+    return "Contents/aarch64-linux";
+  #else // older ARM versions
     // The VST3 documentation says:
     // the architecture name based on the output of command-line
     // "uname -m" (machine hardware) + "-linux"
