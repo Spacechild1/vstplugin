da5a2b8c663c2b78e8bf9e5c311b13524afd1a74
Christof Ressi <christof.ressi@gmx.at>
2024-06-05 12:33:51 +0200

    (temporarily) bump version to 0.6.1-test1

--- vst/Interface.h
+++ vst/Interface.h
@@ -79,8 +79,8 @@ namespace vst {
 
 const int VERSION_MAJOR = 0;
 const int VERSION_MINOR = 6;
-const int VERSION_PATCH = 0;
-const int VERSION_PRERELEASE = 0;
+const int VERSION_PATCH = 1;
+const int VERSION_PRERELEASE = 1;
 
 const char * getVersionString();
 
